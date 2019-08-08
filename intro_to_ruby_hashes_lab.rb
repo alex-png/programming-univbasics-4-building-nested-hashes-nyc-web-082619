372575f3643e9a80552457e01bb747d0be58f1d6
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-08 20:47:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,14 @@ end
 
 def monopoly_with_third_tier
   {
-  :railroads => {:pieces => 4, :rent_in_dollars => {:key_1, :key_2, :key_3, :key_4  }
+  :railroads => {
+    :pieces => 4, 
+    :rent_in_dollars => {
+      :key_1 => "money", 
+      :key_2 => "money", 
+      :key_3 => "money", 
+      :key_4 => "money"
+      }}
     
   }
 
